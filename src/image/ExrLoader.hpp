fdd68d6cb3d8a3e8b85014d0820cbed6c2a4af57
Matt Pharr <matt@pharr.org>
2024-11-11 10:37:20 -0800

    Fix build issues with OpenEXR and Sixel
    - Add SIXEL_INCLUDE_DIRS to the target_include_directories for vv
    - Just #include ImgRgbaFile.h to get the RgbaInputFile definition;
      with a local install of Imf_3_3, for example, hardcoding to
      Imf_3_2 causes build failures.

--- src/image/ExrLoader.hpp
+++ src/image/ExrLoader.hpp
@@ -2,13 +2,13 @@
 
 #include <memory>
 
+#include <ImfRgbaFile.h>
+
 #include "util/FileWrapper.hpp"
 #include "util/NoCopy.hpp"
 
 class Bitmap;
 class ExrStream;
-
-namespace Imf_3_2 { class RgbaInputFile; }
 
 class ExrLoader
 {
@@ -23,7 +23,7 @@ public:
 
 private:
     std::unique_ptr<ExrStream> m_stream;
-    std::unique_ptr<Imf_3_2::RgbaInputFile> m_exr;
+    std::unique_ptr<Imf::RgbaInputFile> m_exr;
 
     bool m_valid;
 };
